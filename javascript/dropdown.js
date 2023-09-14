3716225a582b0af5c1a7684cceb0f271b4c9ca0c
Emma Creteur <emma_creteur@hotmail.com>
2023-09-14 14:52:04 +0200

    responsive

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -21,7 +21,7 @@ if (!event.target.matches('.dropbtn')) {
 
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("mySidebar").style.width = "500px";
   document.getElementById("menu-responsive").style.marginLeft = "250px";
 }
 
